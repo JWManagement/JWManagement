7b3442b574d7258c10cf0537a5df78954414782c
Marvin Zeising <marvin.zeising@gmail.com>
2017-02-03 20:29:06 +0100

    add mobile config file

---
+++ mobile-config.js
@@ -0,0 +1,25 @@
+App.info({
+  id: 'org.jwmanagement',
+  name: 'JW Management',
+  description: 'A highly configurable shift management system for Jehovah\'s Witnesses. Built to power projects like the metropolitan witnessing, construction projects and/or similar',
+  author: 'JW Developer',
+  email: 'support@jwmanagement.org',
+  website: 'https://www.jwmanagement.org'
+});
+
+App.icons({
+  'iphone_2x': 'public/favicon/apple-touch-icon-120x120.png',
+  'iphone_3x': 'public/favicon/apple-touch-icon-180x180.png',
+  'ipad': 'public/favicon/apple-touch-icon-76x76.png',
+  'ipad_2x': 'public/favicon/apple-touch-icon-152x152.png',
+  'ipad_pro': 'public/favicon/apple-touch-icon-180x180.png'
+});
+
+//App.launchScreens({});
+
+App.setPreference('BackgroundColor', '0xff3f51b5');
+App.setPreference('HideKeyboardFormAccessoryBar', true);
+App.setPreference('Orientation', 'default');
+App.setPreference('Orientation', 'all', 'ios');
+
+App.accessRule("blob:*");
