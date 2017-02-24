cc9eba8de446551dd3dbcad19780552662409f82
Tim Antkowiak <antkowiak.tim@gmail.com>
2017-02-24 19:13:53 +0100

    added icons for android

--- mobile-config.js
+++ mobile-config.js
@@ -12,7 +12,12 @@ App.icons({
   'iphone_3x': 'public/favicon/apple-touch-icon-180x180.png',
   'ipad': 'public/favicon/apple-touch-icon-76x76.png',
   'ipad_2x': 'public/favicon/apple-touch-icon-152x152.png',
-  'ipad_pro': 'public/favicon/apple-touch-icon-180x180.png'
+  'ipad_pro': 'public/favicon/apple-touch-icon-180x180.png',
+  'android_mdpi': 'public/favicon/android-chrome-48x48.png',
+  'android_hdpi': 'public/favicon/android-chrome-72x72.png',
+  'android_xhdpi': 'public/favicon/android-chrome-96x96.png',
+  'android_xxhdpi': 'public/favicon/android-chrome-144x144.png',
+  'android_xxxhdpi': 'public/favicon/android-chrome-192x192.png'
 });
 
 //App.launchScreens({});
