13ce75a0106e651a115e8deb56ed083a28ebbdfa
Ellis Marshall <ellisnmarshall@gmail.com>
2017-02-25 09:26:04 +0000

    Added additional icons & launch screen images for iPhone.

--- mobile-config.js
+++ mobile-config.js
@@ -13,14 +13,21 @@ App.icons({
   'ipad': 'public/favicon/apple-touch-icon-76x76.png',
   'ipad_2x': 'public/favicon/apple-touch-icon-152x152.png',
   'ipad_pro': 'public/favicon/apple-touch-icon-180x180.png',
+  'ios_settings': 'public/favicon/apple-touch-icon-29x29.png',
+  'ios_settings_2x': 'public/favicon/apple-touch-icon-58x58.png',
   'android_mdpi': 'public/favicon/android-chrome-48x48.png',
   'android_hdpi': 'public/favicon/android-chrome-72x72.png',
   'android_xhdpi': 'public/favicon/android-chrome-96x96.png',
   'android_xxhdpi': 'public/favicon/android-chrome-144x144.png',
-  'android_xxxhdpi': 'public/favicon/android-chrome-192x192.png'
+  'android_xxxhdpi': 'public/favicon/android-chrome-192x192.png',
 });
 
-//App.launchScreens({});
+App.launchScreens({
+  'iphone_2x': 'public/favicon/iphone4.png',
+  'iphone5': 'public/favicon/iphone5.png',
+  'iphone6': 'public/favicon/iphone6.png',
+  'iphone6p_portrait': 'public/favicon/iphone6plus.png',
+});
 
 App.setPreference('BackgroundColor', '0xff3f51b5');
 App.setPreference('HideKeyboardFormAccessoryBar', true);
