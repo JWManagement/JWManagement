c49fc4d8d3f437bcfa9ef9b58bd35ec5caab43cd
Daniel Miedzik <daniel.miedzik@gmail.com>
2017-10-29 20:24:28 +0100

    fix polish translations and add datepicker trans

---
+++ client/lib/datepicker/bootstrap-datepicker.pl.js
@@ -0,0 +1,16 @@
+import './bootstrap-datepicker.js'
+
+!function (a) {
+    a.fn.datepicker.dates.de = {
+        days: ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"],
+        daysShort: ["Nie", "Pon", "Wto", "Śro", "Czw", "Pią", "Sob"],
+        daysMin: ["Nd", "Pn", "Wt", "Śr", "Cz", "Pt", "Sb"],
+        months: ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"],
+        monthsShort: ["Sty", "Lut", "Mar", "Kwi", "Maj", "Cze", "Lip", "Sie", "Wrz", "Paź", "Lis", "Gru"],
+        today: "Dziś",
+        monthsTitle: "Miesiące",
+        clear: "Wyczyść",
+        weekStart: 1,
+        format: "dd.mm.yyyy"
+    }
+}(jQuery)
