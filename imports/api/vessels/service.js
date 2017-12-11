94cfa3c1354b74f059ec15a73775b0b400e7f7de
Marvin Zeising <marvin.zeising@gmail.com>
2017-12-11 18:03:31 +0100

    finish permission checks for vessel service methods

--- imports/api/vessels/service.js
+++ imports/api/vessels/service.js
@@ -27,43 +27,6 @@ Meteor.methods({
         .reduce(() => getExtendedVessel(vesselId), {})[key];
     },
     'vessel.insert': ({}, vessel) => {
-        // TODO: verify that user has permissions
-        try {
-            new PersistenceManager(Vessels).insert(vessel);
-            return vessel._id;
-        } catch(e) {
-            throw new Meteor.Error(e);
-        }
-    },
-    'vessel.update': (vesselId, key, value) => {
-        // TODO: verify that user has permissions
-        try {
-            new PersistenceManager(Vessels).update(vesselId, key, value);
-        } catch(e) {
-            throw new Meteor.Error(e);
-        }
-    },
-    'vessel.visit.insert': ({ projectId, entityId }, visit) => {
-        // TODO: verify that user has permissions
-
-        visit.projectId = projectId; // TODO: verify that is vessel project
-        delete visit.userId
-
-        const vessel = Vessels.findOne(entityId);
-        let visits = vessel.visits;
-
-        visits.push(visit);
-
-        try {
-            new PersistenceManager(Vessels).update(vessel._id, 'visits', visits); // TODO: add push method
-            return vessel._id;
-        } catch(e) {
-            throw new Meteor.Error(e);
-        }
-    },
-    'vessel.visit.getAvailableHarbors': ({ projectId }) => {
-        // TODO: verify that user has permissions
-
         const project = Projects.findOne(projectId, {
             fields: {
                 vesselModule: 1,
@@ -72,58 +35,64 @@ Meteor.methods({
         });
 
         if (project != null && project.vesselModule) {
-            return project.harbors.map(({ _id, name}) => {
-                return {
-                    key: _id,
-                    value: name
-                }
-            });
-        }
-
-        return [];
-    },
-    'vessel.visit.get': ({ vesselId, visitId }) => {
-        const projectIds = GetGroupsForUser(Meteor.userId(), Permissions.member);
-        let returnVisit = {};
-
-        // TODO: only for admin
-        return [];
-
-        Projects.find({
-            _id: {
-                $in: projectIds
+            try {
+                new PersistenceManager(Vessels).insert(vessel);
+                return vessel._id;
+            } catch(e) {
+                throw new Meteor.Error(e);
             }
-        }, {
+        }
+    },
+    'vessel.update': (vesselId, key, value) => {
+        const project = Projects.findOne(projectId, {
+            fields: {
+                vesselModule: 1,
+                harbors: 1
+            }
+        });
+
+        if (project != null && project.vesselModule) {
+            try {
+                new PersistenceManager(Vessels).update(vesselId, key, value);
+            } catch(e) {
+                throw new Meteor.Error(e);
+            }
+        }
+    },
+    'vessel.visit.insert': ({ projectId, vesselId }, visit) => {
+        const project = Projects.findOne(projectId, {
+            fields: {
+                vesselModule: 1,
+                harbors: 1
+            }
+        });
+
+        if (project != null && project.vesselModule) {
+            const visits = Vessels.findOne(vesselId).visits;
+
+            visit.projectId = projectId;
+            delete visit.userId
+            visits.push(visit);
+
+            try {
+                new PersistenceManager(Vessels).update(vessel._id, 'visits', visits);
+                return vessel._id;
+            } catch(e) {
+                throw new Meteor.Error(e);
+            }
+        }
+    },
+    'vessel.visit.getAvailableHarbors': ({ projectId }) => {
+        return Projects.find(projectId, {
             fields: {
                 vesselModule: 1,
                 harbors: 1
             }
         })
         .fetch()
-        .some((project) => {
-            if (project.vesselModule) {
-                returnVisit = Vessels.find(vesselId, {
-                    fields: {
-                        visits: 1
-                    }
-                })
-                .fetch()
-                .map((vessel) => {
-                    if (vessel.visits != null) {
-                        return vessel.visits;
-                    } else {
-                        return [];
-                    }
-                })
-                .reduce((acc, vessel) => acc.concat(vessel.visits))
-                .filter((visit) => visit._id == visitId)
-                .reduce((acc, visit) => visit);
-
-                return true;
-            }
-        });
-
-        return returnVisit;
+        .filter((project) => project.vesselModule)
+        .reduce((acc, project) => acc.concat(project.harbors), [])
+        .map(({_id, name}) => { return { key: _id, value: name } });
     },
     'vessel.visit.getLast': ({ vesselId }) => {
         return Projects.find({
