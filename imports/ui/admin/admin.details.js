b8c7ca1648f64f80504ec4374d52933455585e0d
Marvin Zeising <marvin.zeising@gmail.com>
2018-03-09 20:36:54 +0100

    add missing admin links to new page

--- imports/ui/admin/admin.details.js
+++ imports/ui/admin/admin.details.js
@@ -1,17 +1,41 @@
+import Permissions from '/imports/api/util/permissions.js';
+
 Template['admin.details'].helpers({
     data() {
         return {
-            backLink: 'dashboard',
+            getMethod: 'admin.get',
+            backLink: 'home',
             sections: [
                 {
                     header: 'administrationSection',
                     contents: [
                         {
                             key: 'settings',
-                            link: 'settings'
+                            link: 'settings',
+                            canSee: Permissions.shiftAdmin
                         }, {
                             key: 'users',
-                            link: 'users.search'
+                            link: 'users',
+                            canSee: Permissions.admin
+                        }, {
+                            key: 'reports',
+                            link: 'reports',
+                            canSee: Permissions.shiftAndStoreAdmin
+                        }, {
+                            key: 'store',
+                            link: 'store',
+                            canSee: Permissions.storeAdmin
+                        }, {
+                            key: 'vessels',
+                            link: 'vessel.search',
+                            canSee: Permissions.member,
+                            custom: (project) => {
+                                return project.vesselModule == true;
+                            }
+                        }, {
+                            key: 'notes',
+                            link: 'notes',
+                            canSee: Permissions.shiftAndStoreAdmin
                         }
                     ]
                 }
