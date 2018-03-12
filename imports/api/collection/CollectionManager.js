f4c0a1085dce644660c8159f789c65aa4e3afb29
Marvin Zeising <marvin.zeising@gmail.com>
2018-03-12 22:59:57 +0100

    add CollectionManager

---
+++ imports/api/collection/CollectionManager.js
@@ -0,0 +1,85 @@
+module.exports = class CollectionManager {
+    constructor(collection, db) {
+        this.collection = collection;
+        this.db = db;
+    }
+
+    validate(entity) {
+        this.collection.schema.clean(entity, { mutate: true });
+        this.collection.schema.validate(entity);
+        this.checkUniqueFields(entity);
+    }
+
+    insert(parentId, entity) {
+        try {
+            this.validate(entity);
+            this.db.update({
+                _id: parentId
+            }, {
+                $addToSet: {
+                    [this.collection.name]: entity
+                }
+            });
+        } catch(e) {
+            throw new Meteor.Error(e.error, e.details);
+        }
+    }
+
+    update(parentId, entityId, key, value) {
+        try {
+            let entity = this.db.findOne(entityId);
+            entity = getUpdatedEntity(key.split('.'), entity, value);
+            this.validate(entity);
+            this.db.update({
+                _id: parentId,
+                [this.collection.name + '._id']: entityId
+            }, {
+                $set: {
+                    [this.collection.name + '.$']: entity
+                }
+            });
+        } catch(e) {
+            throw new Meteor.Error(e.error, e.details);
+        }
+    }
+
+    delete(entity) {}
+
+    checkUniqueFields(parentId, entity) {
+        const errors = [];
+
+        this.collection.uniqueKeys.forEach((key) => {
+            if (key in entity) {
+                const existingEntitiesCount = this.db
+                    .findOne(parentId)[this.collection.name]
+                    .filter((collectionEntity) => {
+                        return collectionEntity[key] == entity[key];
+                    })
+                    .count();
+
+                if (existingEntitiesCount > 0) {
+                    errors.push({
+                        name: key,
+                        type: 'unique'
+                    });
+                }
+            }
+        });
+
+        if (errors.length > 0) {
+            throw new ValidationError(errors);
+        }
+    }
+}
+
+function getUpdatedEntity(keys, entity, value) {
+    const firstKey = keys.shift();
+
+    if (keys.length > 0) {
+        entity[firstKey] = getUpdatedEntity(keys, entity[firstKey], value);
+        return entity;
+    }
+
+    entity[firstKey] = value;
+    return entity;
+}
