521759c59dbe7400893b61bdabad323c95efe58e
Marvin Zeising <marvin.zeising@gmail.com>
2018-04-05 00:31:03 -0400

    move prerender.io config to imports

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -3,6 +3,7 @@ import '/imports/api/messages/messages.coffee';
 import '/imports/api/reports/reports.coffee';
 
 import '/imports/startup/server/mailer.js';
+import '/imports/startup/server/prerenderio.js';
 
 import '/imports/api/admin/service.js';
 import '/imports/api/users/service.js';
--- server/main.js
+++ server/main.js
@@ -1,8 +1,1 @@
-Meteor.startup(() => {
-	prerenderio = Npm.require('prerender-node')
-	prerenderio.set('prerenderToken', 'z2nMr1jIoWf5M4aKY8bN')
-	prerenderio.set('protocol', 'https')
-	WebApp.rawConnectHandlers.use(prerenderio)
-});
-
 import '/imports/startup/server/index.js'
