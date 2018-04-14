11f18008c74cd97ceed1130244baf7f41e92ee3e
Marvin Zeising <marvin.zeising@gmail.com>
2018-04-14 15:10:11 +0200

    use js style for object

--- imports/ui/users/user.insert.js
+++ imports/ui/users/user.insert.js
@@ -3,34 +3,31 @@ Template['user.insert'].helpers({
         return {
             backLink: 'user.search',
             entityKey: 'userId',
-            fields: [
-                {
-                    key: 'profile_firstname'
-                }, {
-                    key: 'profile_lastname'
-                }, {
-                    key: 'profile_email'
-                }, {
-                    key: 'profile_telefon'
-                }, {
-                    key: 'username'
-                }, {
-                    key: 'profile_gender',
-                    type: 'dropdown'
-                }, {
-                    key: 'profile_bdate',
-                    type: 'date',
-                    format: 'DD.MM.YYYY'
-                }, {
-                    key: 'profile_pioneer',
-                    type: 'dropdown'
-                }, {
-                    key: 'profile_privilege',
-                    type: 'dropdown'
-                }, {
-                    key: 'profile_languages'
-                }
-            ]
+            fields: [{
+                key: 'profile_firstname'
+            }, {
+                key: 'profile_lastname'
+            }, {
+                key: 'profile_email'
+            }, {
+                key: 'profile_telefon'
+            }, {
+                key: 'username'
+            }, {
+                key: 'profile_gender',
+                type: 'dropdown'
+            }, {
+                key: 'profile_bdate',
+                type: 'date',
+            }, {
+                key: 'profile_pioneer',
+                type: 'dropdown'
+            }, {
+                key: 'profile_privilege',
+                type: 'dropdown'
+            }, {
+                key: 'profile_languages'
+            }]
         }
     }
 });
