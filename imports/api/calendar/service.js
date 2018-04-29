3e33317dab527defcc23a6f14205871021d80366
Marvin Zeising <marvin.zeising@gmail.com>
2018-04-29 22:37:58 +0200

    implement basic shift search with tag name filling

--- imports/api/calendar/service.js
+++ imports/api/calendar/service.js
@@ -1,21 +1,61 @@
 import Permissions from '/imports/api/util/Permissions.js'
 
 Meteor.methods({
-    'calendar.getShifts': ({ projectId }) => {
-        const project = Projects.findOne(projectId, {
-            fields: {
-                vesselModule: 1
-            }
+    'calendar.getShifts': ({ projectId, date }) => {
+        checkPermissions(projectId);
+
+        const project = Projects.findOne(projectId, { fields: { tags: 1 } });
+
+        const tagIds = Roles.getGroupsForUser(Meteor.userId()).filter((tagId) => {
+            return project.tags.map((tag) => {
+                return tag._id;
+            })
+            .indexOf(tagId) > -1;
         });
 
-        if (project == null) {
-            throw new Meteor.Error('projectNotFound');
+        const shifts = Shifts.find({
+            projectId: projectId,
+            tagId: {
+                $in: tagIds
+            },
+            date: date
+        }, {
+            fields: {
+                date: 1,
+                start: 1,
+                end: 1,
+                status: 1,
+                tagId: 1,
+                'teams._id': 1
+            },
+            sort: {
+                start: 1,
+                end: 1,
+                tagId: 1
+            }
+        })
+        .fetch();
+
+        for (let shift of shifts) {
+            for (let tag of project.tags) {
+                if (tag._id == shift.tagId) {
+                    shift.tag = tag.name;
+                }
+            }
         }
 
-        if (!Roles.userIsInRole(Meteor.userId(), Permissions.member, projectId)) {
-            throw new Meteor.Error('youAreNoProjectMember');
-        }
-
-        return project;
+        return shifts;
     }
 });
+
+function checkPermissions(projectId) {
+    const project = Projects.findOne(projectId, { fields: { _id: 1 } });
+
+    if (project == null) {
+        throw new Meteor.Error('projectNotFound');
+    }
+
+    if (!Roles.userIsInRole(Meteor.userId(), Permissions.member, projectId)) {
+        throw new Meteor.Error('userNotProjectMember');
+    }
+}
