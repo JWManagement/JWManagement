b6655069c384f5da810f0b5ad3c0bc7d43d1c867
Marvin Zeising <marvin.zeising@gmail.com>
2018-05-12 10:45:31 +0200

    remove the old support page

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -13,10 +13,6 @@ import '/imports/api/vessels/service.js';
 import '/imports/api/notes/service.js';
 import '/imports/api/calendar/service.js';
 
-import '/imports/ui/components/allUsers/server/support.users.coffee';
-import '/imports/ui/components/allProjects/server/support.projects.coffee';
-import '/imports/ui/components/enquiryList/server/support.messages.coffee';
-
 import SimpleSchemaHelper from '/imports/api/util/SimpleSchemaHelper.js';
 SimpleSchemaHelper.init();
 
