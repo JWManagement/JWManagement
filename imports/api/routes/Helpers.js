439bf82e0022e8dad91b82e1837f8f7bf702d641
Marvin Zeising <marvin.zeising@gmail.com>
2018-05-13 18:17:20 +0200

    remove debug logs

--- imports/api/routes/Helpers.js
+++ imports/api/routes/Helpers.js
@@ -7,18 +7,13 @@ const Helpers = {
             const language = FlowRouter.current().params.language;
             const myLanguage = Meteor.user().profile.language;
 
-            console.log(language);
-            console.log(myLanguage);
-
             if (language != myLanguage) {
-                console.log('update url');
                 FlowRouter.withReplaceState(() => {
                     FlowRouter.setParams({ language: myLanguage });
                 });
             }
 
             if (TAPi18n.getLanguage() != myLanguage) {
-                console.log('set tap language');
                 TAPi18n.setLanguage(myLanguage);
                 moment.locale(myLanguage);
             }
--- imports/startup/client/language.js
+++ imports/startup/client/language.js
@@ -6,18 +6,13 @@ Meteor.startup(() => {
             const language = FlowRouter.current().params.language;
             const myLanguage = Meteor.user().profile.language;
 
-            console.log(language);
-            console.log(myLanguage);
-
             if (language != myLanguage) {
-                console.log('update url');
                 FlowRouter.withReplaceState(() => {
                     FlowRouter.setParams({ language: myLanguage });
                 });
             }
 
             if (TAPi18n.getLanguage() != myLanguage) {
-                console.log('set tap language');
                 TAPi18n.setLanguage(myLanguage);
                 moment.locale(myLanguage);
             }
