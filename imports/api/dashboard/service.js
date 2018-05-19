95d4aa7c003c8ffee71b66175831ce17288224d7
Marvin Zeising <marvin.zeising@gmail.com>
2018-05-19 08:29:05 +0200

    add pending requests

--- imports/api/dashboard/service.js
+++ imports/api/dashboard/service.js
@@ -1,30 +1,50 @@
 import Permissions from '/imports/api/util/Permissions.js';
 
+const PROJECT_OPTIONS = {
+    fields: {
+        name: 1,
+        news: 1,
+        tags: 1,
+        vesselModule: 1
+    },
+    sort: {
+        name: 1,
+        _id: 1
+    }
+};
+
+const SHIFT_OPTIONS = {
+    fields: {
+        projectId: 1,
+        tagId: 1,
+        date: 1,
+        start: 1,
+        end: 1,
+        teams: 1
+    },
+    sort: {
+        date: 1,
+        start: 1,
+        end: 1,
+        tagId: 1
+    }
+};
+
 Meteor.methods({
     'dashboard.get': ({ projectId }) => {
         const userId = Meteor.userId();
         const projectIds = Roles.getAllGroupsForUser(userId, Permissions.member);
         const today = parseInt(moment().format('YYYYDDD'));
-
         const projects = getProjects(projectIds);
         const missingShiftReports = getMissingShiftReports(projectIds, projects, today, userId);
         const upcomingShifts = getUpcomingShifts(projectIds, projects, today, userId);
+        const pendingRequests = getPendingRequests(projectIds, projects, today, userId);
 
         return {
-            myProjects: projects.map((project) => {
-                delete project.name;
-                delete project.tags;
-                return project;
-            }),
-            missingShiftReports: missingShiftReports.map((shift) => {
-                delete shift.tagId;
-                delete shift.teams;
-                return shift;
-            }),
-            upcomingShifts: upcomingShifts.map((shift) => {
-                delete shift.tagId;
-                return shift;
-            })
+            myProjects: getCleanedProjects(projects),
+            missingShiftReports: getCleanedShifts(missingShiftReports),
+            upcomingShifts: getCleanedShifts(upcomingShifts),
+            pendingRequests: getCleanedShifts(pendingRequests)
         };
     }
 });
@@ -34,18 +54,7 @@ function getProjects(projectIds) {
         _id: {
             $in: projectIds
         }
-    }, {
-        fields: {
-            name: 1,
-            news: 1,
-            tags: 1,
-            vesselModule: 1
-        },
-        sort: {
-            name: 1,
-            _id: 1
-        }
-    })
+    }, PROJECT_OPTIONS)
     .fetch()
     .map((project) => {
         project.project = project.name;
@@ -54,29 +63,7 @@ function getProjects(projectIds) {
 }
 
 function getMissingShiftReports(projectIds, projects, date, userId) {
-    console.log({
-        projectId: {
-            $in: projectIds
-        },
-        date: {
-            $lt: date
-        },
-        teams: {
-            participants: {
-                $elemMatch: {
-                    _id: userId,
-                    thisTeamleader: true
-                }
-            },
-            $not: {
-                $elemMatch: {
-                    'report.submitted': true
-                }
-            }
-        }
-    });
-
-    return Shifts.find({
+    const shifts = Shifts.find({
         projectId: {
             $in: projectIds
         },
@@ -89,32 +76,50 @@ function getMissingShiftReports(projectIds, projects, date, userId) {
                 thisTeamleader: true
             }
         }
-    }, {
-        fields: {
-            projectId: 1,
-            tagId: 1,
-            date: 1,
-            start: 1,
-            end: 1,
-            teams: 1
-        },
-        sort: {
-            date: 1,
-            start: 1,
-            end: 1,
-            tagId: 1
-        }
-    })
+    }, SHIFT_OPTIONS)
     .fetch()
     .filter((shift) => {
-        console.log(shift);
-
         // only return shift if my team hasn't submitted the report yet
         return shift.teams.filter((team) => {
             return team.report == null || !team.report.submitted;
         }).length > 0;
-    })
-    .map((shift) => {
+    });
+
+    return getUpdatedShifts(projects, shifts);
+}
+
+function getUpcomingShifts(projectIds, projects, date, userId) {
+    const shifts = Shifts.find({
+        projectId: {
+            $in: projectIds
+        },
+        date: {
+            $gte: date
+        },
+        'teams.participants._id': userId
+    }, SHIFT_OPTIONS)
+    .fetch();
+
+    return getUpdatedShifts(projects, shifts);
+}
+
+function getPendingRequests(projectIds, projects, date, userId) {
+    const shifts = Shifts.find({
+        projectId: {
+            $in: projectIds
+        },
+        date: {
+            $gte: date
+        },
+        'teams.pending._id': userId
+    }, SHIFT_OPTIONS)
+    .fetch();
+
+    return getUpdatedShifts(projects, shifts);
+}
+
+function getUpdatedShifts(projects, shifts) {
+    return shifts.map((shift) => {
         const project = projects.filter((project) => {
             return project._id == shift.projectId;
         })[0];
@@ -130,43 +135,18 @@ function getMissingShiftReports(projectIds, projects, date, userId) {
     });
 }
 
-function getUpcomingShifts(projectIds, projects, date, userId) {
-    return Shifts.find({
-        projectId: {
-            $in: projectIds
-        },
-        date: {
-            $gte: date
-        },
-        'teams.participants._id': userId
-    }, {
-        fields: {
-            projectId: 1,
-            tagId: 1,
-            date: 1,
-            start: 1,
-            end: 1
-        },
-        sort: {
-            date: 1,
-            start: 1,
-            end: 1,
-            tagId: 1
-        }
-    })
-    .fetch()
-    .map((shift) => {
-        const project = projects.filter((project) => {
-            return project._id == shift.projectId;
-        })[0];
-
-        const tag = project.tags.filter((tag) => {
-            return tag._id == shift.tagId;
-        })[0];
-
-        shift.tag = tag.name;
-        shift.date = parseInt(moment(shift.date, 'YYYYDDD').format('YYYYMMDD'));
-
+function getCleanedShifts(shifts) {
+    return shifts.map((shift) => {
+        delete shift.tagId;
+        delete shift.teams;
         return shift;
+    });
+}
+
+function getCleanedProjects(projects) {
+    return projects.map((project) => {
+        delete project.name;
+        delete project.tags;
+        return project;
     });
 }
