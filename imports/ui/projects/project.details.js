406cca2672a8ac6dbe94ca9c043411ebb9fb0f8e
Marvin Zeising <marvin.zeising@gmail.com>
2018-08-04 13:57:04 +0200

    add icons to project details page

--- imports/ui/projects/project.details.js
+++ imports/ui/projects/project.details.js
@@ -1,71 +1,80 @@
 import Permissions from '/imports/api/util/Permissions.js';
 
 Template['project.details'].helpers({
-    data: {
-        getMethod: 'project.get',
-        navigation: {
-            backLink: 'dashboard.details',
-            navbarStyle: 'flat',
-            hideTitle: true
-        },
-        sections: [{
-            type: 'header',
-            title: 'name',
-            description: 'news_text'
-        }, {
-            title: 'modules',
-            contents: [{
-                key: 'calendar',
-                type: 'link',
-                route: 'calendar',
-                canSee: Permissions.member
-            }, {
-                key: 'shifts',
-                type: 'link',
-                route: 'shifts',
-                canSee: Permissions.member
-            }, {
-                key: 'knowledgeBase',
-                type: 'link',
-                route: 'wiki',
-                canSee: Permissions.member
-            }, {
-                key: 'vessels',
-                type: 'link',
-                route: 'vessel.search',
-                canSee: Permissions.member,
-                custom: (project) => {
-                    return project.vesselModule == true;
-                }
-            }]
-        }, {
-            title: 'administration',
-            contents: [{
-                key: 'settings',
-                type: 'link',
-                route: 'settings',
-                canSee: Permissions.shiftAdmin
-            }, {
-                key: 'publishers',
-                type: 'link',
-                route: 'publisher.search',
-                canSee: Permissions.admin
-            }, {
-                key: 'reports',
-                type: 'link',
-                route: 'reports',
-                canSee: Permissions.shiftAndStoreAdmin
-            }, {
-                key: 'store',
-                type: 'link',
-                route: 'store',
-                canSee: Permissions.storeAdmin
-            }, {
-                key: 'notes',
-                type: 'link',
-                route: 'note.details',
-                canSee: Permissions.shiftAndStoreAdmin
-            }]
-        }]
-    }
+  data: {
+  getMethod: 'project.get',
+  navigation: {
+    backLink: 'dashboard.details',
+    navbarStyle: 'flat',
+    hideTitle: true
+  },
+  sections: [{
+    type: 'header',
+    title: 'name',
+    description: 'news_text'
+  }, {
+    title: 'modules',
+    contents: [{
+      key: 'calendar',
+      type: 'link',
+      route: 'calendar',
+      icon: 'today',
+      canSee: Permissions.member
+    }, {
+      key: 'shifts',
+      type: 'link',
+      route: 'shifts',
+      icon: 'date_range',
+      canSee: Permissions.member
+    }, {
+      key: 'knowledgeBase',
+      type: 'link',
+      route: 'wiki',
+      icon: 'school',
+      canSee: Permissions.member
+    }, {
+      key: 'vessels',
+      type: 'link',
+      route: 'vessel.search',
+      icon: 'directions_boat',
+      canSee: Permissions.member,
+      custom: (project) => {
+        return project.vesselModule == true;
+      }
+    }]
+  }, {
+    title: 'administration',
+    contents: [{
+      key: 'settings',
+      type: 'link',
+      route: 'settings',
+      icon: 'settings',
+      canSee: Permissions.shiftAdmin
+    }, {
+      key: 'publishers',
+      type: 'link',
+      route: 'publisher.search',
+      icon: 'people',
+      canSee: Permissions.admin
+    }, {
+      key: 'reports',
+      type: 'link',
+      route: 'reports',
+      icon: 'question_answer',
+      canSee: Permissions.shiftAndStoreAdmin
+    }, {
+      key: 'store',
+      type: 'link',
+      route: 'store',
+      icon: 'storage',
+      canSee: Permissions.storeAdmin
+    }, {
+      key: 'notes',
+      type: 'link',
+      route: 'note.details',
+      icon: 'note',
+      canSee: Permissions.shiftAndStoreAdmin
+    }]
+  }]
+  }
 });
