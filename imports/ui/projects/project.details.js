075f382d3f5546db9f2360ded524874c6d84241f
Marvin Zeising <marvin.zeising@gmail.com>
2018-08-04 21:03:05 +0200

    go back to dashboard after project leaving

--- imports/ui/projects/project.details.js
+++ imports/ui/projects/project.details.js
@@ -2,89 +2,90 @@ import Permissions from '/imports/api/util/Permissions.js';
 
 Template['project.details'].helpers({
   data: {
-  getMethod: 'project.get',
-  navigation: {
-    backLink: 'dashboard.details',
-    navbarStyle: 'flat',
-    hideTitle: true
-  },
-  sections: [{
-    type: 'header',
-    title: 'name',
-    description: 'news_text'
-  }, {
-    title: 'modules',
-    contents: [{
-    key: 'calendar',
-    type: 'link',
-    route: 'calendar',
-    icon: 'today',
-    canSee: Permissions.member
+    getMethod: 'project.get',
+    navigation: {
+      backLink: 'dashboard.details',
+      navbarStyle: 'flat',
+      hideTitle: true
+    },
+    sections: [{
+      type: 'header',
+      title: 'name',
+      description: 'news_text'
     }, {
-    key: 'shifts',
-    type: 'link',
-    route: 'shifts',
-    icon: 'date_range',
-    canSee: Permissions.member
+      title: 'modules',
+      contents: [{
+        key: 'calendar',
+        type: 'link',
+        route: 'calendar',
+        icon: 'today',
+        canSee: Permissions.member
+      }, {
+        key: 'shifts',
+        type: 'link',
+        route: 'shifts',
+        icon: 'date_range',
+        canSee: Permissions.member
+      }, {
+        key: 'knowledgeBase',
+        type: 'link',
+        route: 'wiki',
+        icon: 'school',
+        canSee: Permissions.member
+      }, {
+        key: 'vessels',
+        type: 'link',
+        route: 'vessel.search',
+        icon: 'directions_boat',
+        canSee: Permissions.member,
+        custom: (project) => {
+          return project.vesselModule == true;
+        }
+      }]
     }, {
-    key: 'knowledgeBase',
-    type: 'link',
-    route: 'wiki',
-    icon: 'school',
-    canSee: Permissions.member
+      title: 'administration',
+      contents: [{
+        key: 'settings',
+        type: 'link',
+        route: 'settings',
+        icon: 'settings',
+        canSee: Permissions.shiftAdmin
+      }, {
+        key: 'publishers',
+        type: 'link',
+        route: 'publisher.search',
+        icon: 'people',
+        canSee: Permissions.admin
+      }, {
+        key: 'reports',
+        type: 'link',
+        route: 'reports',
+        icon: 'question_answer',
+        canSee: Permissions.shiftAndStoreAdmin
+      }, {
+        key: 'store',
+        type: 'link',
+        route: 'store',
+        icon: 'storage',
+        canSee: Permissions.storeAdmin
+      }, {
+        key: 'notes',
+        type: 'link',
+        route: 'note.details',
+        icon: 'note',
+        canSee: Permissions.shiftAndStoreAdmin
+      }]
     }, {
-    key: 'vessels',
-    type: 'link',
-    route: 'vessel.search',
-    icon: 'directions_boat',
-    canSee: Permissions.member,
-    custom: (project) => {
-      return project.vesselModule == true;
-    }
+      title: 'participation',
+      contents: [],
+      actions: [{
+        key: 'leave',
+        type: 'confirm',
+        style: 'danger',
+        method: 'project.leave',
+        route: 'dashboard.details',
+        icon: 'meeting_room'
+      }]
     }]
-  }, {
-    title: 'administration',
-    contents: [{
-    key: 'settings',
-    type: 'link',
-    route: 'settings',
-    icon: 'settings',
-    canSee: Permissions.shiftAdmin
-    }, {
-    key: 'publishers',
-    type: 'link',
-    route: 'publisher.search',
-    icon: 'people',
-    canSee: Permissions.admin
-    }, {
-    key: 'reports',
-    type: 'link',
-    route: 'reports',
-    icon: 'question_answer',
-    canSee: Permissions.shiftAndStoreAdmin
-    }, {
-    key: 'store',
-    type: 'link',
-    route: 'store',
-    icon: 'storage',
-    canSee: Permissions.storeAdmin
-    }, {
-    key: 'notes',
-    type: 'link',
-    route: 'note.details',
-    icon: 'note',
-    canSee: Permissions.shiftAndStoreAdmin
-    }]
-  }, {
-    title: 'participation',
-    contents: [],
-    actions: [{
-    key: 'leave',
-    type: 'confirm',
-    style: 'danger',
-    method: 'project.leave',
-    icon: 'meeting_room'
-    }]
-  }]
   }
 });
