cadfc3cdc2ab69fc538893dbd2b436c9f4879fa8
Marvin Zeising <marvin.zeising@gmail.com>
2018-10-23 14:26:27 +0200

    only connect to monti in production

--- imports/startup/server/monti.js
+++ imports/startup/server/monti.js
@@ -1,3 +1,6 @@
+import { Meteor } from 'meteor/meteor'
 import { Monti } from 'meteor/montiapm:agent'
 
-Monti.connect('7FfwpEeySTRn4QFBC', '0c620f39-7751-4dda-a9d6-685738ef5a4c')
+if (Meteor.isProduction) {
+  Monti.connect('7FfwpEeySTRn4QFBC', '0c620f39-7751-4dda-a9d6-685738ef5a4c')
+}
