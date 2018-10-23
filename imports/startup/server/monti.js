dbae8012eeff917c1cbd35ca820648d771df465c
Marvin Zeising <marvin.zeising@gmail.com>
2018-10-23 14:14:19 +0200

    fix standard js issues

--- imports/startup/server/monti.js
+++ imports/startup/server/monti.js
@@ -1,1 +1,3 @@
-Kadira.connect('7FfwpEeySTRn4QFBC', '0c620f39-7751-4dda-a9d6-685738ef5a4c')
+import { Monti } from 'meteor/montiapm:agent'
+
+Monti.connect('7FfwpEeySTRn4QFBC', '0c620f39-7751-4dda-a9d6-685738ef5a4c')
