e91f39f852f81e03cc56b0c7b236b46f8d9fee9d
Marvin Zeising <marvin.zeising@gmail.com>
2018-10-24 03:43:50 +0200

    add missing old logic to update the shifts and set the teamleader permissions there; #272
    took it from: https://github.com/JWDeveloper/JWManagement/blob/9f866d500cf6d95d65862bb44f9a3e4597734f42/both/methods/user/userPermissions.coffee

--- imports/api/publishers/service.permissions.js
+++ imports/api/publishers/service.permissions.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor'
 import { Roles } from 'meteor/alanning:roles'
-
+import moment from 'moment'
 import Permissions from '../../framework/Constants/Permissions'
 import { checkPermissions } from '../../framework/Functions/Security'
 
@@ -86,6 +86,226 @@ function publisherPermissionsTagUpdate ({ projectId, userId, tagId }, key, value
 
     if (value !== 'none') {
       Roles.setUserRoles(userId, value, tagId)
+    }
+
+    let i
+    let j
+    let k
+    let l
+    let len
+    let len1
+    let len2
+    let len3
+    let len4
+    let m
+    let ref
+    let ref1
+    let setSubstituteTeamleader
+    let setTeamleader
+    let shift
+    let shifts
+    let team
+    let u
+    let updatedDeclined
+    let updatedParticipants
+    let updatedPending
+    let user
+    let indexOf = [].indexOf
+    const permission = value
+
+    let tagPermissions = Permissions.participant
+
+    const project = Projects.findOne({ 'tags._id': tagId }, { fields: { _id: 1 } })
+
+    if (indexOf.call(tagPermissions, permission) >= 0) {
+      if (Roles.userIsInRole(userId, tagPermissions, tagId)) {
+        Roles.removeUsersFromRoles(userId, tagPermissions, tagId)
+        if (permission === 'teamleader') {
+          setTeamleader = true
+          setSubstituteTeamleader = false
+        } else if (permission === 'substituteTeamleader') {
+          setTeamleader = false
+          setSubstituteTeamleader = true
+        } else if (permission === 'participant') {
+          setTeamleader = false
+          setSubstituteTeamleader = false
+        }
+        shifts = Shifts.find({
+          projectId: project._id,
+          tagId: tagId,
+          $or: [
+            {
+              'teams.participants._id': userId
+            },
+            {
+              'teams.pending._id': userId
+            },
+            {
+              'teams.declined._id': userId
+            }
+          ]
+        })
+
+        ref = shifts.fetch()
+
+        for (i = 0, len = ref.length; i < len; i++) {
+          shift = ref[i]
+          ref1 = shift.teams
+
+          for (j = 0, len1 = ref1.length; j < len1; j++) {
+            team = ref1[j]
+
+            if (indexOf.call((function () {
+              var k, len2, ref2, results
+              ref2 = team.participants
+              results = []
+              for (k = 0, len2 = ref2.length; k < len2; k++) {
+                u = ref2[k]
+                results.push(u._id)
+              }
+              return results
+            })(), userId) >= 0) {
+              updatedParticipants = team.participants
+
+              for (k = 0, len2 = updatedParticipants.length; k < len2; k++) {
+                user = updatedParticipants[k]
+                if (!(user._id === userId)) {
+                  continue
+                }
+                user.teamleader = setTeamleader
+                user.substituteTeamleader = setSubstituteTeamleader
+              }
+
+              Shifts.update({
+                _id: shift._id,
+                'teams._id': team._id
+              }, {
+                $set: {
+                  'teams.$.participants': updatedParticipants
+                }
+              })
+            }
+            if (indexOf.call((function () {
+              var l, len3, ref2, results
+              ref2 = team.pending
+              results = []
+
+              for (l = 0, len3 = ref2.length; l < len3; l++) {
+                u = ref2[l]
+                results.push(u._id)
+              }
+              return results
+            })(), userId) >= 0) {
+              updatedPending = team.pending
+              for (l = 0, len3 = updatedPending.length; l < len3; l++) {
+                user = updatedPending[l]
+                if (!(user._id === userId)) {
+                  continue
+                }
+                user.teamleader = setTeamleader
+                user.substituteTeamleader = setSubstituteTeamleader
+              }
+              Shifts.update({
+                _id: shift._id,
+                'teams._id': team._id
+              }, {
+                $set: {
+                  'teams.$.pending': updatedPending
+                }
+              })
+            }
+            if (indexOf.call((function () {
+              var len4, m, ref2, results
+              ref2 = team.declined.filter(function (u) {
+                return u != null
+              })
+              results = []
+              for (m = 0, len4 = ref2.length; m < len4; m++) {
+                u = ref2[m]
+                results.push(u._id)
+              }
+              return results
+            })(), userId) >= 0) {
+              updatedDeclined = team.participants
+              for (m = 0, len4 = updatedDeclined.length; m < len4; m++) {
+                user = updatedDeclined[m]
+                if (!(user._id === userId)) {
+                  continue
+                }
+                user.teamleader = setTeamleader
+                user.substituteTeamleader = setSubstituteTeamleader
+              }
+              Shifts.update({
+                _id: shift._id,
+                'teams._id': team._id
+              }, {
+                $set: {
+                  'teams.$.declined': updatedDeclined
+                }
+              })
+            }
+          }
+        }
+      }
+
+      Roles.addUsersToRoles(userId, permission, tagId)
+    } else if (permission === 'none') {
+      if (Roles.userIsInRole(userId, tagPermissions, tagId)) {
+        Roles.removeUsersFromRoles(userId, tagPermissions, tagId)
+
+        Shifts.update({
+          projectId: project._id,
+          tagId: tagId,
+          date: {
+            $gt: parseInt(moment(new Date()).format('YYYYDDDD'))
+          },
+          'teams.participants._id': userId
+        }, {
+          $pull: {
+            'teams.$.participants': {
+              _id: userId
+            }
+          }
+        }, {
+          multi: true
+        })
+
+        Shifts.update({
+          projectId: project._id,
+          tagId: tagId,
+          date: {
+            $gt: parseInt(moment(new Date()).format('YYYYDDDD'))
+          },
+          'teams.pending._id': userId
+        }, {
+          $pull: {
+            'teams.$.pending': {
+              _id: userId
+            }
+          }
+        }, {
+          multi: true
+        })
+
+        Shifts.update({
+          projectId: project._id,
+          tagId: tagId,
+          date: {
+            $gt: parseInt(moment(new Date()).format('YYYYDDDD'))
+          },
+          'teams.declined._id': userId
+        }, {
+          $pull: {
+            'teams.$.declined': {
+              _id: userId
+            }
+          }
+        }, {
+          multi: true
+        })
+      }
+    } else {
+      throw new Meteor.Error(500, 'Role ' + permission + ' invalid')
     }
 
     return true
