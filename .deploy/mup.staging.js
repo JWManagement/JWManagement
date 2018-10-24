b6f2a567db4f529ffe1df70e911aca9f879c9c25
Marvin Zeising <marvin.zeising@gmail.com>
2018-10-24 03:21:39 +0200

    revert mup beanstalk tests

--- .deploy/mup.staging.js
+++ .deploy/mup.staging.js
@@ -1,32 +1,40 @@
 module.exports = {
+  servers: {
+    one: {
+      host: process.env.SERVER_IP,
+      username: 'root',
+      password: process.env.SERVER_PASSWORD
+    }
+  },
   app: {
-    type: 'aws-beanstalk',
     name: 'JWManagementStaging',
     path: '../',
+    docker: {
+      image: 'zodern/meteor:root'
+    },
+    servers: {
+      one: {}
+    },
     env: {
       ROOT_URL: 'https://staging.jwmanagement.org',
       MONGO_URL: process.env.MONGO_URL,
       MONGO_OPLOG_URL: process.env.MONGO_OPLOG_URL,
       MAIL_URL: process.env.MAIL_URL,
+      APM_ID: process.env.APM_ID,
+      APM_KEY: process.env.APM_KEY,
       AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
       AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
       LOGGLY_TOKEN: process.env.LOGGLY_TOKEN
     },
-    auth: {
-      id: process.env.AWS_ACCESS_KEY_ID,
-      secret: process.env.AWS_SECRET_ACCESS_KEY
-    },
-    minInstances: 1,
-    maxInstances: 10,
-    instanceType: 't3.micro',
-    sslDomains: ['staging.jwmanagement.org'],
-    forceSSL: true,
-    region: 'eu-central-1',
-    gracefulShutdown: false,
-    oldVersions: 3,
     buildOptions: {
-      buildLocation: '../build'
-    }
+      serverOnly: true
+    },
+    enableUploadProgressBar: false
   },
-  plugins: ['mup-aws-beanstalk']
+  proxy: {
+    domains: 'staging.jwmanagement.org',
+    ssl: {
+      letsEncryptEmail: 'support@jwmanagement.org'
+    }
+  }
 };
