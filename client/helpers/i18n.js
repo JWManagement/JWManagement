1e669c1c4ec970388606918c2277767a043861cf
Marvin Zeising <marvin.zeising@gmail.com>
2018-11-10 12:17:51 +0100

    add global _ template helper

--- client/helpers/i18n.js
+++ client/helpers/i18n.js
@@ -5,3 +5,5 @@ import SystemLanguages from '../../imports/framework/Constants/SystemLanguages'
 Template.registerHelper('langTag', () => i18next.language)
 
 Template.registerHelper('getLanguages', () => SystemLanguages.allowedValues)
+
+Template.registerHelper('_', (key, options) => i18next.t(key, options))
