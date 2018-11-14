852f7df89bf670429226d259320adcb8fa14c7c3
Marvin Zeising <marvin.zeising@gmail.com>
2018-11-14 22:27:35 +0100

    use styled-components to replace custom "fake" styles

--- imports/ui/shiftLoading/ShiftLoading.jsx
+++ imports/ui/shiftLoading/ShiftLoading.jsx
@@ -1,20 +1,45 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const ShiftHeader = styled.div`
+  width: 60px;
+  height: 17px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: rgba(255, 255, 255, 0.7);
+`
+
+const TeamHeader = styled.div`
+  width: 80px;
+  height: 12px;
+  margin-top: 4px;
+  margin-bottom: 4px;
+  background-color: rgba(32, 41, 96, 0.4);
+`
+
+const Text = styled.div`
+  width: 100px;
+  height: 12px;
+  margin-top: 6px;
+  margin-bottom: 6px;
+  background-color: rgba(32, 41, 96, 0.3);
+`
 
 function ShiftLoading () {
   return (
-    <button className='btn btn-block shift fake-shift'>
+    <button className='btn btn-block shift'>
       <h3 className='shift-period'>
-        <div className='fake-header' />
+        <ShiftHeader />
       </h3>
       <div className='shift-teams'>
         <div className='shift-team'>
           <div className='shift-team-header'>
-            <div className='fake-team-header' />
+            <TeamHeader />
           </div>
           <div className='shift-team-participants'>
-            <div className='fake-text' />
-            <div className='fake-text' />
-            <div className='fake-text' />
+            <Text />
+            <Text />
+            <Text />
           </div>
         </div>
       </div>
