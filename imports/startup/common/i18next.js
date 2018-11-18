fbaafa3f15d9a45191cee2654502b9221056ac7b
Marvin Zeising <marvin.zeising@gmail.com>
2018-11-18 19:15:31 +0100

    add language detector plugin

--- imports/startup/common/i18next.js
+++ imports/startup/common/i18next.js
@@ -1,11 +1,14 @@
 import i18next from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import en from '../../i18n/en/_export'
+import de from '../../i18n/de/_export'
 
-i18next.init({
-  fallbackLng: 'en',
-  resources: {
-    en: {
-      translation: en
+i18next
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: 'en',
+    resources: {
+      en: { translation: en },
+      de: { translation: de }
     }
-  }
-})
+  })
