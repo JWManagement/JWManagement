0c2f9a44b89e5529834d4099832bbbf964864163
Marvin Zeising <marvin.zeising@gmail.com>
2018-11-18 21:06:22 +0100

    pass options as param or count depending on the type of the parameter

--- client/helpers/i18n.js
+++ client/helpers/i18n.js
@@ -6,4 +6,13 @@ Template.registerHelper('langTag', () => i18next.language)
 
 Template.registerHelper('getLanguages', () => SystemLanguages.allowedValues)
 
-Template.registerHelper('_', (key, options) => i18next.t(key, options))
+Template.registerHelper('_', (key, options) => {
+  console.log(options)
+  if (typeof options === 'string') {
+    options = { param: options }
+  } else if (typeof options === 'number') {
+    options = { count: options }
+  }
+
+  return i18next.t(key, options)
+})
