50154d2c60916ce9ed82477a11a9c0bead59bc41
Marvin Zeising <marvin.zeising@gmail.com>
2018-12-04 20:20:05 +0100

    migrate chinese (simplified) translation

--- imports/startup/common/i18next.js
+++ imports/startup/common/i18next.js
@@ -4,6 +4,7 @@ import en from '../../i18n/en/_export'
 import de from '../../i18n/de/_export'
 import it from '../../i18n/it/_export'
 import zhTW from '../../i18n/zh-TW/_export'
+import zhCN from '../../i18n/zh-CN/_export'
 
 i18next
   .use(LanguageDetector)
@@ -13,6 +14,7 @@ i18next
       en: { translation: en },
       de: { translation: de },
       it: { translation: it },
-      'zh-TW': { translation: zhTW }
+      'zh-TW': { translation: zhTW },
+      'zh-CN': { translation: zhCN }
     }
   })
