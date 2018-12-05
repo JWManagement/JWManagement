c02b03654c5d89ab0f59816e8361e43817b3f2bf
Marvin Zeising <marvin.zeising@gmail.com>
2018-12-05 16:48:57 +0100

    migrate hungarian translation

--- imports/startup/common/i18next.js
+++ imports/startup/common/i18next.js
@@ -4,6 +4,7 @@ import en from '../../i18n/en/_export'
 import de from '../../i18n/de/_export'
 import fi from '../../i18n/fi/_export'
 import fr from '../../i18n/fr/_export'
+import hu from '../../i18n/hu/_export'
 import it from '../../i18n/it/_export'
 import zhTW from '../../i18n/zh-TW/_export'
 import zhCN from '../../i18n/zh-CN/_export'
@@ -17,6 +18,7 @@ i18next
       de: { translation: de },
       fi: { translation: fi },
       fr: { translation: fr },
+      hu: { translation: hu },
       it: { translation: it },
       'zh-TW': { translation: zhTW },
       'zh-CN': { translation: zhCN }
