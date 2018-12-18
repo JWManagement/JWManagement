c5cab9a8c8ca1b69b04b11ebd5163a212aedf62a
Marvin Zeising <marvin.zeising@gmail.com>
2018-12-18 11:56:24 +0100

    preload languages only on server-side

--- imports/startup/common/i18next.js
+++ imports/startup/common/i18next.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor'
 import i18next from 'i18next'
 import Backend from 'i18next-node-locize-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
@@ -8,7 +9,7 @@ i18next
   .use(LanguageDetector)
   .init({
     fallbackLng: 'en',
-    preload: SystemLanguages.allowedValues,
+    preload: Meteor.isServer ? SystemLanguages.allowedValues : false,
     backend: {
       projectId: 'd0390b1e-95ad-4aa6-93b8-6372476039d8',
       apiKey: '5bb8eba8-deff-4a65-8a2a-fd7029d9a16b',
