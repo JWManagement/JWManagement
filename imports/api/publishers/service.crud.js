5ff3ec634c7bbb45f0f145e3b1a5c13c39114ead
Marvin Zeising <marvin.zeising@gmail.com>
2019-06-17 12:22:06 +0200

    add missing import

--- imports/api/publishers/service.crud.js
+++ imports/api/publishers/service.crud.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor'
+import { Random } from 'meteor/random'
 import SimpleSchema from 'simpl-schema'
 import objectAssignDeep from 'object-assign-deep'
 import { checkPermissions } from '../../framework/Functions/Security'
