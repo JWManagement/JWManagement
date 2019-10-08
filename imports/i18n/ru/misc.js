0024d7d83aaeed9cc4d816ef84bbae4c057a029e
Alex <25442872+alexlaux@users.noreply.github.com>
2019-10-08 14:50:17 +0200

    Update misc.js
    Time translation corrected.

--- imports/i18n/ru/misc.js
+++ imports/i18n/ru/misc.js
@@ -41,7 +41,7 @@ const misc = {
   time: {
     start: 'Начало',
     end: 'Конец',
-    suffix: 'Время',
+    suffix: 'Чс',
     years: 'год',
     to: 'до'
   },
@@ -79,9 +79,9 @@ const misc = {
     coordinator: 'КCC',
     secretary: 'Секр.',
     serviceOverseer: 'СH',
-    elder: 'С',
+    elder: 'Стар.',
     servant: 'СП',
-    publisher: 'В'
+    publisher: 'Возв.'
   },
   period: {
     d: 'Д',
