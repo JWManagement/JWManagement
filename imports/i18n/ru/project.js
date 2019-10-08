13d0f171a16c542598ee010d6cf9ba80297573bd
Alex <25442872+alexlaux@users.noreply.github.com>
2019-10-08 11:27:12 +0200

    Create project.js

---
+++ imports/i18n/ru/project.js
@@ -0,0 +1,54 @@
+const project = {
+  nameShort: 'Verwaltung',
+  entity: {
+    _id: 'ID',
+    name: 'Name',
+    email: 'Projekt E-Mail Adresse',
+    news: {
+      text: 'Neuigkeiten'
+    },
+    shifts: 'Schichten',
+    calendar: 'Kalender',
+    knowledgeBase: 'Informationen',
+    settings: 'Einstellungen',
+    users: 'Benutzer',
+    publishers: 'Verkündiger',
+    publisherActions: 'Verkündiger Massenfunktionen',
+    reports: 'Berichte',
+    store: 'Depot',
+    vessels: 'Schiffe',
+    notes: 'Notizen',
+    leave: 'Teilnahme an diesem Projekt aufkündigen',
+    leaveConfirmation: 'Möchtest du dieses Projekt wirklich verlassen und JEGLICHE TEILNAHME DARAN AUFKÜNDIGEN? Das kann nicht rückgängig gemacht werden!',
+    supportPage: 'Hilfe',
+    support: {
+      phone: 'Ruf an',
+      discord: 'Schreib uns auf Discord (empfohlen)',
+      github: 'Lege ein GitHub Issue an',
+      paypal: 'PayPal',
+      iban: 'IBAN'
+    },
+    noElements: 'Keine Projekte gefunden'
+  },
+  details: {
+    sections: {
+      project: 'Projekt',
+      modules: 'Module',
+      administration: 'Verwaltung',
+      participation: 'Teilnahme'
+    }
+  },
+  search: {
+    placeholder: 'Suche nach Projekten'
+  },
+  support: {
+    details: {
+      sections: {
+        support: 'Hilfe bekommen',
+        donate: 'Unterstützen'
+      }
+    }
+  }
+}
+
+export default project
