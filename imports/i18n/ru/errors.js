c146a12c331c1921362977d96d6389fa04e15a1b
Alex <25442872+alexlaux@users.noreply.github.com>
2019-10-27 11:06:42 +0100

    Additional translations v2.0.3

--- imports/i18n/ru/errors.js
+++ imports/i18n/ru/errors.js
@@ -1,9 +1,16 @@
 const error = {
+  '403': 'Es existiert bereits ein Account mit diesem Benutzernamen',
   nameRequired: 'Введите название этого корабля',
   callsignUnique: 'Корабль с этим позывным уже существует',
   eniUnique: 'Корабль с этим номером ENI уже существует',
   imoUnique: 'Корабль с этим номером IMO уже существует',
-  mmsiUnique: 'Корабль с этим MMSI-номером уже существует'
+  mmsiUnique: 'Корабль с этим MMSI-номером уже существует',
+  missingField: 'Пожалуйста, заполни все поля',
+  incorrectPassword: 'Этот пароль не совпадает',
+  noAccountFound: 'Нет пользователя с таким именем или этим адресом электронной почты',
+  multipleAccountsFound: 'Есть несколько пользователей с этим адресом электронной почты. Пожалуйста, используйте имя пользователя',
+  passwordsDoNotMatch: 'Пароли не совпадают',
+  passwordTooShort: 'Пароль должен иметь не менее 8 символов'
 }
 
 export default error
